e7500fc3d3d34523f1cda60bde7715f445030384
Dennis.Nguyen <dennis.nguyen@wisetechglobal.com>
2026-05-14 18:10:43 +1000

    simple HTTP request/response using node:http

--- projects/02_simple_web_server/src/index.js
+++ projects/02_simple_web_server/src/index.js
@@ -1,10 +1,16 @@
 const http = require('node:http');
 
 const port = process.env.PORT || 3000;
+const [node_cmd, script_path, command, ...args] = process.argv;
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-  response.end('Hello World\n');
+  const message = `Hello World\n`
+    + ` Your command line arguments are ${JSON.stringify(process.argv)}\n`
+    + ` Your command is ${command}\n`
+    + ` Your arguments are ${JSON.stringify(args)}\n`;
+  response.end(message);
+
 });
 
 server.listen(port, () => {
